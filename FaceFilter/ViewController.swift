eb8c4db047f0080fb9908f3146c9ad87280b017f
Philipp <pd95@users.noreply.github.com>
2020-03-20 09:30:59 +0100

    Initial Commit

---
+++ FaceFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceFilter
+//
+//  Created by Philipp on 20.03.20.
+//  Copyright © 2020 Philipp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
