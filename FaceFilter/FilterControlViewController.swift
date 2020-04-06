79c13cc152ff515ee128d4f5bca912b66285be7b
Philipp <pd95@users.noreply.github.com>
2020-04-07 01:04:24 +0200

    Added face filter preview image when editing settings

--- FaceFilter/FilterControlViewController.swift
+++ FaceFilter/FilterControlViewController.swift
@@ -16,7 +16,8 @@ class FilterControlViewController: UIViewController {
     @IBOutlet private weak var inputScale: UISlider!
     @IBOutlet private weak var overshootAmountLabel: UILabel!
     @IBOutlet private weak var overshootAmount: UISlider!
-
+    @IBOutlet weak var facePreview: UIImageView!
+    
     private let model = AppModel.shared
     
     override func viewDidLoad() {
@@ -30,12 +31,17 @@ class FilterControlViewController: UIViewController {
         inputScale.value = floor(inputScale.value)
         inputScaleLabel.text = "\(inputScale.value)"
         model.currentParameterValue = inputScale.value
+
+        model.blurHeads()
+        refreshPreview()
     }
 
     @IBAction func overshootAmountChanged(_ sender: UISlider) {
         overshootAmount.value = round(overshootAmount.value * 10) / 10
         overshootAmountLabel.text = "\(overshootAmount.value)"
         model.overshootAmount = CGFloat(overshootAmount.value)
+        model.calculateMask()
+        refreshPreview()
     }
 
     @IBAction func filterSelected(_ sender: UISegmentedControl) {
@@ -46,6 +52,26 @@ class FilterControlViewController: UIViewController {
         inputScaleLabel.text = "\(inputScale.value)"
         overshootAmount.value = Float(model.overshootAmount)
         overshootAmountLabel.text = "\(overshootAmount.value)"
+        
+        model.blurHeads()
+        refreshPreview()
+    }
+    
+    func refreshPreview() {
+        if let faceRect = model.detectedFaceRect.first, let outputImage = model.outputImage {
+            facePreview.image = nil
+            print("faceRect = \(faceRect)")
+            let size = facePreview.bounds.size
+            print("Preview size = \(size)")
+            let dx = (faceRect.size.width - size.width)/2
+            let dy = (faceRect.size.height - size.height)/2
+            print("dx = \(dx)")
+            print("dy = \(dy)")
+            let cropRect = faceRect.insetBy(dx: dx, dy: dy)
+            print("Crop size = \(cropRect)")
+            let image = outputImage.cropped(to: cropRect)
+            facePreview.image = UIImage(ciImage: image)
+        }
     }
     
     @IBAction func dismiss() {
